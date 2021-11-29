5269f4681131720e42e9a541b684ebf0dae62dbc
Bajo 3D <bajo3d@gmail.com>
2021-11-29 23:35:58 +0800

    Penambahan section team,contact, dan footer

--- script.js
+++ script.js
@@ -5,10 +5,57 @@ $(document).ready(function () {
     } else {
       $('.navbar').removeClass('sticky');
     }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
   });
+  // slide-up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+  });
+
   //   toggle menu/navbar script
   $('.menu-btn').click(function () {
     $('.navbar .menu').toggleClass('active');
     $('.menu-btn i').toggleClass('active');
+  });
+
+  // typing animation script
+  var typed = new Typed('.typing', {
+    strings: ['Web Design', 'Develoer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed('.typing-2', {
+    strings: ['Web Design', 'Develoer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel skript
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
